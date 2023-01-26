e2f7e9953c372a339324e692130ef0db90865f4c
DESKTOP-IN4R64E\strac <strachev@gmail.com>
2023-01-26 13:30:45 +0200

    set count button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(1);
+
+  const onClick = (e) => {
+    setCount(count + 1);
+  }
+
+  useEffect(() => {
+    document.title = "Count (" + count + ")";
+  }, [count]);
+  
   return (
     <div className="App">
+      <button onclick={onClick}>Count ({count})</button>
       <section class="hero">
         <div class="hero-body">
           <p class="title">A React Task</p>
